2e8e454e19f59a6adca9cbdd4de5308f229bcf62
stanleykubrik1 <62182076+stanleykubrik1@users.noreply.github.com>
2021-08-05 16:09:10 +0200

    aside fix

--- src/js/aside.js
+++ src/js/aside.js
@@ -28,12 +28,14 @@ const repositoryTitle = document.querySelector(".link-title");
 
 
 
-console.log(about);
+console.log(technologies);
 
 btnBack.addEventListener("click", (e) => {
     aside.style.display = "none";
     document.body.style.overflow = "scroll";
     pageMask.classList.remove("mask");
+    technologiesUl.innerHTML = ""
+    
 });
 
 linkBack.addEventListener("click", (e) => {
@@ -41,10 +43,27 @@ linkBack.addEventListener("click", (e) => {
     document.body.style.overflow = "scroll";
     pageMask.classList.remove("mask");
 });
+
+
 btnsArray[0].addEventListener("click", (e) => {
     aside.style.display = "block";
     document.body.style.overflow = "hidden";
     pageMask.classList.add("mask");
+
+    const newLi1 = document.createElement("li");
+    technologiesUl.appendChild(newLi1);
+    const textLi1 = document.createTextNode("HTML");
+    newLi1.appendChild(textLi1)
+
+    const newLi2 = document.createElement("li");
+    technologiesUl.appendChild(newLi2);
+    const textLi2 = document.createTextNode("CSS");
+    newLi2.appendChild(textLi2)
+
+    const newLi3 = document.createElement("li");
+    technologiesUl.appendChild(newLi3);
+    const textLi3 = document.createTextNode("JAVASCRIPT");
+    newLi3.appendChild(textLi3)
 });
 
 btnsArray[1].addEventListener("click", (e) => {
@@ -56,19 +75,31 @@ btnsArray[1].addEventListener("click", (e) => {
     overview.innerHTML = " Test 2 Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ratione nam corrupti eos consectetur laudantium atque adipisci, rerum modi, laborum dicta magnam debitis vel similique ullam cumque beatae, provident dolorem dignissimos!";
     img.src = "src/img/Zrzut ekranu 2021-07-29 o 21.23.48 (2).jpeg";
     about.innerHTML = "TEST 2 Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ratione nam corrupti eos consectetur laudantium atque adipisci, rerum modi, laborum dicta magnam debitis vel similique ullam cumque beatae, provident dolorem dignissimos!";
-    technologies[0].innerHTML = "React";
-    technologies[1].innerHTML = "JavaScript";
-    technologies[2].innerHTML = "HTML";
 
     const newLi1 = document.createElement("li");
     technologiesUl.appendChild(newLi1);
-    const textLi1 = document.createTextNode("CSS");
+    const textLi1 = document.createTextNode("React");
     newLi1.appendChild(textLi1)
 
     const newLi2 = document.createElement("li");
     technologiesUl.appendChild(newLi2);
-    const textLi2 = document.createTextNode("BOOTSTRAP")
+    const textLi2 = document.createTextNode("JavaScript");
     newLi2.appendChild(textLi2)
+
+    const newLi3 = document.createElement("li");
+    technologiesUl.appendChild(newLi3);
+    const textLi3 = document.createTextNode("HTML");
+    newLi3.appendChild(textLi3)
+
+    const newLi4 = document.createElement("li");
+    technologiesUl.appendChild(newLi4);
+    const textLi4 = document.createTextNode("CSS");
+    newLi4.appendChild(textLi4)
+
+    const newLi5 = document.createElement("li");
+    technologiesUl.appendChild(newLi5);
+    const textLi5 = document.createTextNode("BOOTSTRAP")
+    newLi5.appendChild(textLi5)
 
     linkRepository.href = "https://github.com/JakubRoethel/Zet_Meble_Calculator_APP";
     linkLiveVersion.href = "https://zetmeble-app.web.app/";
@@ -83,24 +114,36 @@ btnsArray[2].addEventListener("click", (e) => {
     overview.innerHTML = " Test 3 Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ratione nam corrupti eos consectetur laudantium atque adipisci, rerum modi, laborum dicta magnam debitis vel similique ullam cumque beatae, provident dolorem dignissimos!";
     img.src = "src/img/Zrzut ekranu 2021-07-29 o 21.14.30 (2).jpeg";
     about.innerHTML = "TEST 3 Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ratione nam corrupti eos consectetur laudantium atque adipisci, rerum modi, laborum dicta magnam debitis vel similique ullam cumque beatae, provident dolorem dignissimos!";
-    technologies[0].innerHTML = "React";
-    technologies[1].innerHTML = "JavaScript";
-    technologies[2].innerHTML = "HTML";
 
     const newLi1 = document.createElement("li");
     technologiesUl.appendChild(newLi1);
-    const textLi1 = document.createTextNode("CSS");
+    const textLi1 = document.createTextNode("React");
     newLi1.appendChild(textLi1)
 
     const newLi2 = document.createElement("li");
     technologiesUl.appendChild(newLi2);
-    const textLi2 = document.createTextNode("BOOTSTRAP")
+    const textLi2 = document.createTextNode("JavaScript")
     newLi2.appendChild(textLi2)
 
     const newLi3 = document.createElement("li");
     technologiesUl.appendChild(newLi3);
-    const textLi3 = document.createTextNode("FIREBASE");
+    const textLi3 = document.createTextNode("HTML");
     newLi3.appendChild(textLi3);
+
+    const newLi4 = document.createElement("li");
+    technologiesUl.appendChild(newLi4);
+    const textLi4 = document.createTextNode("CSS");
+    newLi4.appendChild(textLi4)
+
+    const newLi5 = document.createElement("li");
+    technologiesUl.appendChild(newLi5);
+    const textLi5 = document.createTextNode("Bootstrap")
+    newLi5.appendChild(textLi5)
+
+    const newLi6 = document.createElement("li");
+    technologiesUl.appendChild(newLi6);
+    const textLi6 = document.createTextNode("Firebase");
+    newLi6.appendChild(textLi6);
 
     linkRepository.href = "https://github.com/JakubRoethel/leaving_it_in_the_good_hands";
     linkLiveVersion.href = "";
@@ -115,24 +158,36 @@ btnsArray[3].addEventListener("click", (e) => {
     overview.innerHTML = " Test 4 Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ratione nam corrupti eos consectetur laudantium atque adipisci, rerum modi, laborum dicta magnam debitis vel similique ullam cumque beatae, provident dolorem dignissimos!";
     img.src = "src/img/Zrzut ekranu 2021-08-5 o 15.08.43.png";
     about.innerHTML = "TEST 4 Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ratione nam corrupti eos consectetur laudantium atque adipisci, rerum modi, laborum dicta magnam debitis vel similique ullam cumque beatae, provident dolorem dignissimos!";
-    technologies[0].innerHTML = "React";
-    technologies[1].innerHTML = "JavaScript";
-    technologies[2].innerHTML = "HTML";
 
     const newLi1 = document.createElement("li");
     technologiesUl.appendChild(newLi1);
-    const textLi1 = document.createTextNode("CSS");
+    const textLi1 = document.createTextNode("React");
     newLi1.appendChild(textLi1)
 
     const newLi2 = document.createElement("li");
     technologiesUl.appendChild(newLi2);
-    const textLi2 = document.createTextNode("BOOTSTRAP")
+    const textLi2 = document.createTextNode("JavaScript")
     newLi2.appendChild(textLi2)
 
     const newLi3 = document.createElement("li");
     technologiesUl.appendChild(newLi3);
-    const textLi3 = document.createTextNode("API");
+    const textLi3 = document.createTextNode("HTML");
     newLi3.appendChild(textLi3);
+
+    const newLi4 = document.createElement("li");
+    technologiesUl.appendChild(newLi4);
+    const textLi4 = document.createTextNode("CSS");
+    newLi4.appendChild(textLi4)
+
+    const newLi5 = document.createElement("li");
+    technologiesUl.appendChild(newLi5);
+    const textLi5 = document.createTextNode("Bootstrap")
+    newLi5.appendChild(textLi5)
+
+    const newLi6 = document.createElement("li");
+    technologiesUl.appendChild(newLi6);
+    const textLi6 = document.createTextNode("API");
+    newLi6.appendChild(textLi6);
 
     linkRepository.href = "https://github.com/JakubRoethel/To_Do_App";
     linkLiveVersion.href = "";
@@ -147,14 +202,26 @@ btnsArray[4].addEventListener("click", (e) => {
     overview.innerHTML = " Test 5 Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ratione nam corrupti eos consectetur laudantium atque adipisci, rerum modi, laborum dicta magnam debitis vel similique ullam cumque beatae, provident dolorem dignissimos!";
     img.src = "src/img/Zrzut ekranu 2021-07-29 o 21.44.39.png";
     about.innerHTML = "TEST 5 Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ratione nam corrupti eos consectetur laudantium atque adipisci, rerum modi, laborum dicta magnam debitis vel similique ullam cumque beatae, provident dolorem dignissimos!";
-    technologies[0].innerHTML = "Wordpress";
-    technologies[1].innerHTML = "HTML";
-    technologies[2].innerHTML = "CSS";
 
     const newLi1 = document.createElement("li");
     technologiesUl.appendChild(newLi1);
-    const textLi1 = document.createTextNode("JavaScript");
+    const textLi1 = document.createTextNode("Wordpress");
     newLi1.appendChild(textLi1)
+
+    const newLi2 = document.createElement("li");
+    technologiesUl.appendChild(newLi2);
+    const textLi2 = document.createTextNode("HTML")
+    newLi2.appendChild(textLi2)
+
+    const newLi3 = document.createElement("li");
+    technologiesUl.appendChild(newLi3);
+    const textLi3 = document.createTextNode("CSS");
+    newLi3.appendChild(textLi3);
+
+    const newLi4 = document.createElement("li");
+    technologiesUl.appendChild(newLi4);
+    const textLi4 = document.createTextNode("JavaScript");
+    newLi4.appendChild(textLi4)
 
     linkRepository.remove();
     repositoryTitle.remove()
@@ -170,19 +237,31 @@ btnsArray[5].addEventListener("click", (e) => {
     overview.innerHTML = " Test 6 Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ratione nam corrupti eos consectetur laudantium atque adipisci, rerum modi, laborum dicta magnam debitis vel similique ullam cumque beatae, provident dolorem dignissimos!";
     img.src = "src/img/Zrzut ekranu 2021-07-29 o 21.47.04.png";
     about.innerHTML = "TEST 6 Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ratione nam corrupti eos consectetur laudantium atque adipisci, rerum modi, laborum dicta magnam debitis vel similique ullam cumque beatae, provident dolorem dignissimos!";
-    technologies[0].innerHTML = "Wordpress";
-    technologies[1].innerHTML = "HTML";
-    technologies[2].innerHTML = "CSS";
 
     const newLi1 = document.createElement("li");
     technologiesUl.appendChild(newLi1);
-    const textLi1 = document.createTextNode("JavaScript");
+    const textLi1 = document.createTextNode("Wordpress");
     newLi1.appendChild(textLi1)
 
     const newLi2 = document.createElement("li");
     technologiesUl.appendChild(newLi2);
-    const textLi2 = document.createTextNode("PHP")
+    const textLi2 = document.createTextNode("HTML")
     newLi2.appendChild(textLi2)
+
+    const newLi3 = document.createElement("li");
+    technologiesUl.appendChild(newLi3);
+    const textLi3 = document.createTextNode("CSS");
+    newLi3.appendChild(textLi3);
+
+    const newLi4 = document.createElement("li");
+    technologiesUl.appendChild(newLi4);
+    const textLi4 = document.createTextNode("JavaScript");
+    newLi4.appendChild(textLi4)
+
+    const newLi5 = document.createElement("li");
+    technologiesUl.appendChild(newLi5);
+    const textLi5 = document.createTextNode("PHP")
+    newLi5.appendChild(textLi5)
 
     linkRepository.remove();
     repositoryTitle.remove()
@@ -194,23 +273,35 @@ btnsArray[6].addEventListener("click", (e) => {
     aside.style.display = "block";
     document.body.style.overflow = "hidden";
     pageMask.classList.add("mask");
-    title.innerHTML = "My Personal PAge";
+    title.innerHTML = "My Personal Page";
     overview.innerHTML = " Test 7 Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ratione nam corrupti eos consectetur laudantium atque adipisci, rerum modi, laborum dicta magnam debitis vel similique ullam cumque beatae, provident dolorem dignissimos!";
     img.src = "src/img/Zrzut ekranu 2021-07-29 o 22.01.11.png";
     about.innerHTML = "TEST 7 Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ratione nam corrupti eos consectetur laudantium atque adipisci, rerum modi, laborum dicta magnam debitis vel similique ullam cumque beatae, provident dolorem dignissimos!";
-    technologies[0].innerHTML = "HTML";
-    technologies[1].innerHTML = "CSS";
-    technologies[2].innerHTML = "SASS";
 
     const newLi1 = document.createElement("li");
     technologiesUl.appendChild(newLi1);
-    const textLi1 = document.createTextNode("JavaScript");
-    newLi1.appendChild(textLi1);
+    const textLi1 = document.createTextNode("HTML");
+    newLi1.appendChild(textLi1)
 
     const newLi2 = document.createElement("li");
     technologiesUl.appendChild(newLi2);
-    const textLi2 = document.createTextNode("Gulp");
-    newLi2.appendChild(textLi2);
+    const textLi2 = document.createTextNode("CSS")
+    newLi2.appendChild(textLi2)
+
+    const newLi3 = document.createElement("li");
+    technologiesUl.appendChild(newLi3);
+    const textLi3 = document.createTextNode("SASS");
+    newLi3.appendChild(textLi3);
+
+    const newLi4 = document.createElement("li");
+    technologiesUl.appendChild(newLi4);
+    const textLi4 = document.createTextNode("JavaScript");
+    newLi4.appendChild(textLi4);
+
+    const newLi5 = document.createElement("li");
+    technologiesUl.appendChild(newLi5);
+    const textLi5 = document.createTextNode("Gulp");
+    newLi5.appendChild(textLi5);
 
 
     linkRepository.href = "https://github.com/JakubRoethel/My_Personal_Page";
